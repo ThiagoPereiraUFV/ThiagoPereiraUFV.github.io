e3c2a55aacff4e9b1cc14385cf7d66e1ded78525
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2024-10-22 16:53:50 -0300

    refactor: Update Home and Projects components
    - Add About component to Home page
    - Update Header component to include dynamic navigation links
    - Fetch GitHub user data and repositories in Home component
    - Add interfaces for GitHub user data and repositories
    - Update Projects component to display a list of repositories with links and descriptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
+import About from "@/components/organisms/About";
 import Footer from "@/components/organisms/Footer";
 import Header from "@/components/organisms/Header";
 import Projects from "@/components/organisms/Projects";
+import { getGithubData } from "@/lib/actions";
 
-export default function Home() {
+export default async function Home() {
+  const githubData = await getGithubData("ThiagoPereiraUFV");
+
+  if ("error" in githubData && githubData.error) {
+    return <div>{githubData.error.message}</div>;
+  }
+
+  const data = {
+    header: {
+      title: "Thiago Pereira",
+      sections: ["About", "Projects", "Contact"],
+    },
+    projects: {
+      repos: githubData.repos,
+    },
+  };
+
   return (
     <main>
-      <Header />
-      <Projects />
+      <Header {...data.header} />
+      <About />
+      <Projects {...data.projects} />
       <Footer />
     </main>
   );
--- src/components/organisms/Projects.tsx
+++ src/components/organisms/Projects.tsx
@@ -1,7 +1,20 @@
-export default function Projects() {
-	return (
-		<section>
-			<h1>Projects</h1>
-		</section>
-	);
+import { IProjectsProps } from "@/interfaces/projects";
+import Link from "next/link";
+
+export default function Projects(props: IProjectsProps) {
+  return (
+    <section>
+      <h2>Projects</h2>
+      <ul>
+        {props.repos.map((repo) => (
+          <li key={repo.id}>
+            <Link href={repo.html_url} target="_blank">
+              {repo.name}
+            </Link>
+            <p>{repo.description}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
