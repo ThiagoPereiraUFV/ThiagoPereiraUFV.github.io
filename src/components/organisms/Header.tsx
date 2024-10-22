c570cee251ddedc81fcd2dade95552e1945a4ab7
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2024-10-22 16:53:20 -0300

    refactor: Update Header component to include dynamic navigation links

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,18 +1,19 @@
 import { IHeaderProps } from "@/interfaces/header";
+import Link from "next/link";
 
 export default function Header(props: IHeaderProps) {
-	return (
-		<header className="tw-grid tw-grid-cols-1 lg:tw-grid-cols-3 tw-justify-between tw-p-4">
-			<h1 className="tw-col-span-2 tw-text-4xl">{props.title}</h1>
-			<nav>
-				<ul className="tw-flex tw-flex-wrap tw-justify-center tw-gap-4">
-					{props.sections.map((section, index) => (
-						<li key={index}>
-							<a href={`#${section.trim().toLowerCase()}`}>{section}</a>
-						</li>
-					))}
-				</ul>
-			</nav>
-		</header>
-	);
+  return (
+    <header className="tw-grid tw-grid-cols-1 lg:tw-grid-cols-3 tw-justify-between tw-p-4">
+      <h1 className="tw-col-span-2 tw-text-4xl">{props.title}</h1>
+      <nav>
+        <ul className="tw-flex tw-flex-wrap tw-justify-center tw-gap-4">
+          {props.sections.map((section, index) => (
+            <li key={index}>
+              <Link href={`#${section.trim().toLowerCase()}`}>{section}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
