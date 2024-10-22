1f44821223a3bdc430bc38a158d687608a137053
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2024-10-22 16:53:10 -0300

    feat: Add actions to fetch GitHub user data and repositories
    This commit adds the `getGithubData`, `getGithubUserData`, and `getGithubUserRepos` functions to the `actions.ts` file. These functions are responsible for fetching data from the GitHub API based on a given username. They handle error cases and return the fetched data or appropriate error messages. This addition enables the application to retrieve and display GitHub user data and repositories.
    
    Ref: #123

---
+++ src/lib/actions.ts
@@ -0,0 +1,111 @@
+'use server';
+
+import { IGithubUserData, IGithubUserRepo } from "@/interfaces/github";
+
+export async function getGithubData(username: string) {
+	try {
+		if (!username) {
+			return {
+				error: {
+					message: "Username is required",
+					status: 400
+				}
+			} as const;
+		}
+
+		const [userData, userRepos] = await Promise.all([
+			getGithubUserData(username),
+			getGithubUserRepos(username)
+		]);
+
+		if ("error" in userData) {
+			return userData;
+		}
+
+		if ("error" in userRepos) {
+			return userRepos;
+		}
+
+		return {
+			...userData,
+			repos: userRepos
+		} as const;
+	} catch (error) {
+		return {
+			error: {
+				message: error instanceof Error ? `Error: ${error.message}` : `Error: ${error}`,
+				status: 500
+			}
+		} as const;
+	}
+}
+
+export async function getGithubUserData(username: string) {
+	try {
+		if (!username) {
+			return {
+				error: {
+					message: "Username is required",
+					status: 400
+				}
+			} as const;
+		}
+
+		const response = await fetch(`https://api.github.com/users/${username}`);
+
+		if (!response.ok) {
+			return {
+				error: {
+					message: "User not found",
+					status: response.status
+				}
+			} as const;
+		}
+
+		const data: IGithubUserData = await response.json();
+
+		return data;
+	} catch (error) {
+		return {
+			error: {
+				message: error instanceof Error ? `Error: ${error.message}` : `Error: ${error}`,
+				status: 500
+			}
+		} as const;
+	}
+}
+
+export async function getGithubUserRepos(username: string) {
+	try {
+		if (!username) {
+			return {
+				error: {
+					message: "Username is required",
+					status: 400
+				}
+			} as const;
+		}
+
+		const response = await fetch(`https://api.github.com/users/${username}/repos`);
+
+		if (!response.ok) {
+			return {
+				error: {
+					message: "User not found",
+					status: response.status
+				}
+			} as const;
+		}
+
+		const data: IGithubUserRepo[] = await response.json();
+
+		return data;
+	} catch (error) {
+		return {
+			error: {
+				message: error instanceof Error ? `Error: ${error.message}` : `Error: ${error}`,
+				status: 500
+			}
+		} as const;
+	}
+}
