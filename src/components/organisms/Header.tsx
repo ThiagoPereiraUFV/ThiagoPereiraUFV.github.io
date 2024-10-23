d4b79d22e5a6bddf6012bc739ad8bd05980465aa
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2024-10-23 14:24:46 -0300

    refactor: Update Header component styles for responsive design

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Header(props: IHeaderProps) {
   return (
-    <header className="tw-grid tw-grid-cols-1 lg:tw-grid-cols-3 tw-justify-between tw-p-4">
+    <header className="tw-grid tw-grid-cols-1 lg:tw-grid-cols-3 tw-justify-between tw-gap-4 tw-px-12 tw-py-10 tw-text-center lg:tw-text-justify">
       <h1 className="tw-col-span-2 tw-text-4xl">{props.title}</h1>
       <nav>
         <ul className="tw-flex tw-flex-wrap tw-justify-center tw-gap-4">
