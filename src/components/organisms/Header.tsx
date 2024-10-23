c8bf435a7f30621a6903eadbee25d0618c354d75
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2024-10-23 15:20:07 -0300

    refactor: Update Header component to adjust padding and alignment in mobile view

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function Header(props: IHeaderProps) {
   return (
-    <header className="tw-grid tw-grid-cols-1 lg:tw-grid-cols-3 tw-justify-between tw-gap-4 tw-px-12 tw-py-10 tw-text-center lg:tw-text-justify">
+    <header className="tw-grid tw-grid-cols-1 lg:tw-grid-cols-3 tw-justify-between tw-gap-4 tw-px-12 tw-py-5 tw-text-center lg:tw-text-justify">
       <h1 className="tw-col-span-2 tw-text-4xl">{props.title}</h1>
       <nav>
-        <ul className="tw-flex tw-flex-wrap tw-justify-center tw-gap-4">
+        <ul className="tw-flex tw-flex-wrap tw-justify-center lg:tw-justify-end tw-gap-4">
           {props.sections.map((section, index) => (
             <li
               className="tw-transition tw-ease-in-out tw-duration-300 hover:tw--translate-y-0.5 hover:tw--translate-x-0.5"
