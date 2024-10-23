ad1ef771af58aa15173198b73ade82d2d13b65de
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2024-10-23 13:38:10 -0300

    refactor: refactor About, Footer, and Projects components

--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -3,6 +3,7 @@ import { IAboutProps } from "@/interfaces/about";
 export default function About(props: IAboutProps) {
   return (
     <section
+      id="about"
       className="tw-px-10 lg:tw-px-20"
       dangerouslySetInnerHTML={{ __html: props.aboutUserData }}
     ></section>
--- src/components/organisms/Projects.tsx
+++ src/components/organisms/Projects.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Projects(props: IProjectsProps) {
   return (
-    <section>
+    <section id="projects">
       <h2>Projects</h2>
       <ul>
         {props.repos.map((repo) => (
