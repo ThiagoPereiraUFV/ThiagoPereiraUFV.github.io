a0dae292ac5f37d8cf4fea54cd848a76376c423f
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2025-02-16 20:06:53 -0300

    fix: ensure n8n-demo component is only required in the browser and improve layout in LowCodeProjects

--- src/components/molecules/N8NWorkflow.tsx
+++ src/components/molecules/N8NWorkflow.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { ILowCodeProject } from "@/interfaces/low-code-projects";
-import "@n8n_io/n8n-demo-component/n8n-demo.bundled.js";
+
+if (typeof window !== "undefined") {
+  require("@n8n_io/n8n-demo-component/n8n-demo.bundled.js");
+}
 
 interface IN8NWorkflowProps {
   key: string;
@@ -10,9 +13,9 @@ interface IN8NWorkflowProps {
 
 export default function N8NWorkflow({ key, workflow }: IN8NWorkflowProps) {
   return (
-    <>
-      <h3>{workflow.name}</h3>
+    <div>
+      <h3 className="tw-text-sm">{workflow.name}</h3>
       <n8n-demo key={key} workflow={JSON.stringify(workflow)}></n8n-demo>
-    </>
+    </div>
   );
 }
