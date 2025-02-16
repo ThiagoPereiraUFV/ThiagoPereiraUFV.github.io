470cfeca5cd1d882bd06b3ac19685bedd2eced30
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2025-02-16 15:40:36 -0300

    fix: update section ID and heading for LowCodeProjects component

--- src/components/organisms/LowCodeProjects.tsx
+++ src/components/organisms/LowCodeProjects.tsx
@@ -4,10 +4,10 @@ import N8NWorkflow from "@/components/molecules/N8NWorkflow";
 export default function LowCodeProjects({ projects }: ILowCodeProjectsProps) {
   return (
     <section
-      id="no-code-projects"
+      id="low-code-projects"
       className="tw-grid tw-grid-cols-1 tw-px-10 lg:tw-px-20 tw-gap-4"
     >
-      <h2 className="tw-text-3xl">No-code/Low-code Projects</h2>
+      <h2 className="tw-text-3xl">Low-code/No-code Projects</h2>
       <div className="tw-grid tw-grid-cols-1 lg:tw-grid-cols-2 tw-gap-4"></div>
       {projects.map((project) => (
         <N8NWorkflow key={project.id} workflow={project} />
