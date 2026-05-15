46dc58f3bf28bb90392a860a7743a9d5ffe8e3b6
ThiagoPereiraUFV <thiago.marinho.pereira.98@gmail.com>
2026-05-15 19:29:04 -0300

    feat: add dynamic export to robots and sitemap components

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { userData } from "@/helpers/userdata";
 
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { userData } from "@/helpers/userdata";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
