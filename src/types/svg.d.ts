f559e8e30a905ba6262270c6aa749b1a991f70c0
ThiagoPereiraUFV <thiago.marinho.pereira.98@gmail.com>
2026-05-22 16:04:31 -0300

    feat: add n8n-demo component support and include necessary polyfills in layout

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -2,3 +2,23 @@ declare module "*.svg" {
 	const content: string;
 	export default content;
 }
+
+declare namespace React {
+	namespace JSX {
+		interface IntrinsicElements {
+			"n8n-demo": React.HTMLAttributes<HTMLElement> & {
+				workflow?: string;
+				frame?: string;
+				src?: string;
+				collapseformobile?: string;
+				clicktointeract?: string;
+				hidecanvaserrors?: string;
+				disableinteractivity?: string;
+				theme?: string;
+				mode?: string;
+				workflowbefore?: string;
+				tidyup?: string;
+			};
+		}
+	}
+}
